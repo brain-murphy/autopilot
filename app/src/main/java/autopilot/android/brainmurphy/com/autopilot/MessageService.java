ed2164e00b2a47e0f445b7cc6a1c46bd7b20b34a
brainmurphy1 <brianmurphy@gatech.edu>
2014-11-18 20:29:36 -0500

    updated to use autopilotapi.taptools.net rest api.

--- app/src/main/java/autopilot/android/brainmurphy/com/autopilot/MessageService.java
+++ app/src/main/java/autopilot/android/brainmurphy/com/autopilot/MessageService.java
@@ -5,6 +5,8 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.net.Uri;
+import android.net.http.AndroidHttpClient;
 import android.os.Binder;
 import android.os.Bundle;
 import android.os.Handler;
@@ -13,16 +15,28 @@ import android.provider.Telephony;
 import android.telephony.SmsManager;
 import android.util.Log;
 
+import java.io.IOException;
+import java.io.StringReader;
+import java.io.UnsupportedEncodingException;
 import java.net.URISyntaxException;
+import java.net.URLEncoder;
 import java.security.InvalidKeyException;
 import java.util.ArrayList;
 import java.util.Map;
+import java.util.Scanner;
 import java.util.Timer;
 import java.util.TimerTask;
 
-import com.microsoft.azure.storage.*;
-import com.microsoft.azure.storage.table.*;
 import android.provider.Settings.Secure;
+
+import org.apache.http.HttpHost;
+import org.apache.http.HttpRequest;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * Created by connorrmounts on 11/15/14.
@@ -41,8 +55,6 @@ public class MessageService extends IntentService {
     private SmsReceiver smsReceiver;
     private IntentFilter intentFilter;
     private String userNumber;
-    private CloudTable messageTable;
-    private CloudTable responseTable;
 
     private Timer timer;
 
@@ -74,96 +86,96 @@ public class MessageService extends IntentService {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                    // Retrieve storage account from connection-string.
-                CloudStorageAccount storageAccount =
-                        null;
-                try {
-                    storageAccount = CloudStorageAccount.parse(storageConnectionString);
-                } catch (URISyntaxException e) {
-                    e.printStackTrace();
-                } catch (InvalidKeyException e) {
-                    e.printStackTrace();
-                }
-
-                // Create the table clients.
-                CloudTableClient tableClient = storageAccount.createCloudTableClient();
-
-                // Create the tables if they doesn't exist.
-                try {
-                    messageTable = new CloudTable("messages",tableClient);
-                } catch (URISyntaxException e) {
-                    e.printStackTrace();
-                } catch (StorageException e) {
-                    e.printStackTrace();
-                }
-                try {
-                    messageTable.createIfNotExists();
-                } catch (StorageException e) {
-                    e.printStackTrace();
-                }
-
-                try {
-                    responseTable = new CloudTable("responses", tableClient);
-                } catch (URISyntaxException e) {
-                    e.printStackTrace();
-                } catch (StorageException e) {
-                    e.printStackTrace();
-                }
-                try {
-                    responseTable.createIfNotExists();
-                } catch (StorageException e) {
-                    e.printStackTrace();
-                }
-
-                timer = new Timer();
-                timer.scheduleAtFixedRate(new TimerTask() {
-                    @Override
-                    public void run() {
-
-                        if (smsManager != null) {
-                            String partitionFilter = TableQuery.generateFilterCondition(PARTITION_KEY,
-                                    TableQuery.QueryComparisons.EQUAL,
-                                    PARTITION);
-
-                            // Specify a partition query, using PARTITION as the partition key filter.
-                            TableQuery<Response> partitionQuery = TableQuery.from(Response.class)
-                                    .where(partitionFilter);
-
-                            ArrayList<Response> list = new ArrayList<Response>();
-
-                            // Loop through the results, displaying information about the entity.
-                            for (Response r : responseTable.execute(partitionQuery)) {
-                                try {
-                                    if (r.getAddress().equals(userNumber)) {
-                                        smsManager.sendTextMessage(r.getRecipient(), null,
-                                                r.getResponse(), null, null);
-
-
-                                        list.add(r);
-                                    }
-
-                                } catch (NullPointerException e) {
-                                    Log.d("NPE", "NullPointer");
-                                }
-                            }
-
-                            for (Response r : list) {
-                                TableOperation deleteResponse = TableOperation.delete(r);
-
-                                try {
-                                    responseTable.execute(deleteResponse);
-                                } catch (StorageException e) {
-                                    e.printStackTrace();
-                                }
-                            }
-                        }
-                    }
-                }, 0, 100);
-            }
-        }).start();
+//        new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                    // Retrieve storage account from connection-string.
+//                CloudStorageAccount storageAccount =
+//                        null;
+//                try {
+//                    storageAccount = CloudStorageAccount.parse(storageConnectionString);
+//                } catch (URISyntaxException e) {
+//                    e.printStackTrace();
+//                } catch (InvalidKeyException e) {
+//                    e.printStackTrace();
+//                }
+//
+//                // Create the table clients.
+//                CloudTableClient tableClient = storageAccount.createCloudTableClient();
+//
+//                // Create the tables if they doesn't exist.
+//                try {
+//                    messageTable = new CloudTable("messages",tableClient);
+//                } catch (URISyntaxException e) {
+//                    e.printStackTrace();
+//                } catch (StorageException e) {
+//                    e.printStackTrace();
+//                }
+//                try {
+//                    messageTable.createIfNotExists();
+//                } catch (StorageException e) {
+//                    e.printStackTrace();
+//                }
+//
+//                try {
+//                    responseTable = new CloudTable("responses", tableClient);
+//                } catch (URISyntaxException e) {
+//                    e.printStackTrace();
+//                } catch (StorageException e) {
+//                    e.printStackTrace();
+//                }
+//                try {
+//                    responseTable.createIfNotExists();
+//                } catch (StorageException e) {
+//                    e.printStackTrace();
+//                }
+//
+//                timer = new Timer();
+//                timer.scheduleAtFixedRate(new TimerTask() {
+//                    @Override
+//                    public void run() {
+//
+//                        if (smsManager != null) {
+//                            String partitionFilter = TableQuery.generateFilterCondition(PARTITION_KEY,
+//                                    TableQuery.QueryComparisons.EQUAL,
+//                                    PARTITION);
+//
+//                            // Specify a partition query, using PARTITION as the partition key filter.
+//                            TableQuery<Response> partitionQuery = TableQuery.from(Response.class)
+//                                    .where(partitionFilter);
+//
+//                            ArrayList<Response> list = new ArrayList<Response>();
+//
+//                            // Loop through the results, displaying information about the entity.
+//                            for (Response r : responseTable.execute(partitionQuery)) {
+//                                try {
+//                                    if (r.getAddress().equals(userNumber)) {
+//                                        smsManager.sendTextMessage(r.getRecipient(), null,
+//                                                r.getResponse(), null, null);
+//
+//
+//                                        list.add(r);
+//                                    }
+//
+//                                } catch (NullPointerException e) {
+//                                    Log.d("NPE", "NullPointer");
+//                                }
+//                            }
+//
+//                            for (Response r : list) {
+//                                TableOperation deleteResponse = TableOperation.delete(r);
+//
+//                                try {
+//                                    responseTable.execute(deleteResponse);
+//                                } catch (StorageException e) {
+//                                    e.printStackTrace();
+//                                }
+//                            }
+//                        }
+//                    }
+//                }, 0, 100);
+//            }
+//        }).start();
         return START_STICKY;
     }
 
@@ -207,33 +219,52 @@ public class MessageService extends IntentService {
                         messages = new android.telephony.SmsMessage[pdus.length];
 
                         messages[0] = android.telephony.SmsMessage.createFromPdu((byte[]) pdus[0]);
-                        Message message = new Message(userNumber);
-                        message.setMessage(messages[0].getDisplayMessageBody());
-                        message.setRecipient(messages[0].getDisplayOriginatingAddress());
+                        //Message message = new Message(userNumber);
+                        String messageIn = messages[0].getDisplayMessageBody();
+                        String sender = messages[0].getDisplayOriginatingAddress();
 
                         String[] enabledContacts = getEnabledContactsNumbers();
-                        TableOperation insertMessage = TableOperation.insertOrReplace(message);
-
-                        if (getAllEnabled()) {
-                            try {
-                                messageTable.execute(insertMessage);
-                                Log.d("MessageUp", "Message Uploaded");
-                            } catch (StorageException e) {
-                                e.printStackTrace();
-                            }
-                        } else if (enabledContacts.length > 0) {
-                            for (String s : enabledContacts) {
-                                if (s.equals(message.getRecipient())) {
-                                    try {
-                                        messageTable.execute(insertMessage);
-                                        Log.d("MessageUp", "Message Uploaded");
-                                    } catch (StorageException e) {
-                                        e.printStackTrace();
-                                    }
-                                    break;
-                                }
-                            }
+                        HttpClient httpClient = new DefaultHttpClient();
+                        HttpHost host = new HttpHost("autopilotapi.taptools.net");
+                        HttpRequest request = null;
+                        try {
+                            request = new HttpGet("/api/message?text="
+                                    + URLEncoder.encode(messageIn, "Utf-8"));
+                        } catch (UnsupportedEncodingException e) {
+                            e.printStackTrace();
                         }
+                        try {
+                            HttpResponse response = httpClient.execute(host, request);
+                            String str = "";
+                            Scanner scn = new Scanner(response.getEntity().getContent());
+                            while (scn.hasNextLine()) {
+                                str += scn.nextLine();
+                            }
+
+                            JSONObject json = new JSONObject(str);
+                            Log.d("json", json.toString());
+                            smsManager.sendTextMessage(sender, null,
+                                    json.getString("Response"), null, null);
+
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+//                        } else if (enabledContacts.length > 0) {
+//                            for (String s : enabledContacts) {
+//                                if (s.equals(message.getRecipient())) {
+//                                    try {
+//                                        messageTable.execute(insertMessage);
+//                                        Log.d("MessageUp", "Message Uploaded");
+//                                    } catch (StorageException e) {
+//                                        e.printStackTrace();
+//                                    }
+////                                    break;
+//                                }
+//                            }
+//                        }
 
 
                         // Create an operation to add the new customer to the people table.
@@ -248,98 +279,98 @@ public class MessageService extends IntentService {
 
     }
 
-    public static class Message extends TableServiceEntity {
-        public String message;
-        public String address;
-        public String recipient;
-        public String modelType;
+//    public static class Message extends TableServiceEntity {
+//        public String message;
+//        public String address;
+//        public String recipient;
+//        public String modelType;
+//
+//        public String getMessage() {
+//            return this.message;
+//        }
+//
+//        public void setMessage(String m) {
+//            this.message = m;
+//        }
+//
+//        public String getAddress() {
+//            return this.address;
+//        }
+//
+//        public void setAddress(String a) {
+//            this.address = a;
+//        }
+//
+//        public String getRecipient() {
+//            return this.recipient;
+//        }
+//
+//        public void setRecipient(String r) {
+//            this.recipient = r;
+//        }
+//
+//        public String getModelType() { return this.recipient; }
+//
+//        public void setModelType(String r) { this.modelType = r; }
 
-        public String getMessage() {
-            return this.message;
-        }
+//        public Message() {
+//            this.partitionKey = PARTITION;
+//            this.rowKey = "ROW";
+//        }
+//
+//        public Message(String row) {
+//            this.partitionKey = PARTITION;
+//            this.rowKey = row;
+//            this.address = row;
+//        }
+//    }
 
-        public void setMessage(String m) {
-            this.message = m;
-        }
-
-        public String getAddress() {
-            return this.address;
-        }
-
-        public void setAddress(String a) {
-            this.address = a;
-        }
-
-        public String getRecipient() {
-            return this.recipient;
-        }
-
-        public void setRecipient(String r) {
-            this.recipient = r;
-        }
-
-        public String getModelType() { return this.recipient; }
-
-        public void setModelType(String r) { this.modelType = r; }
-
-        public Message() {
-            this.partitionKey = PARTITION;
-            this.rowKey = "ROW";
-        }
-
-        public Message(String row) {
-            this.partitionKey = PARTITION;
-            this.rowKey = row;
-            this.address = row;
-        }
-    }
-
-    public static class Response extends TableServiceEntity {
-        public String response;
-        public String address;
-        public String recipient;
-        public String modelType;
-
-        public String getResponse() {
-            return this.response;
-        }
-
-        public void setResponse(String m) {
-            this.response = m;
-        }
-
-        public String getAddress() {
-            return this.address;
-        }
-
-        public void setAddress(String a) {
-            this.address = a;
-        }
-
-        public String getRecipient() {
-            return this.recipient;
-        }
-
-        public void setRecipient(String r) {
-            this.recipient = r;
-        }
-
-        public String getModelType() { return this.recipient; }
-
-        public void setModelType(String r) { this.modelType = r; }
-
-        public Response() {
-            this.partitionKey = PARTITION;
-            this.rowKey = "ROW";
-        }
-
-        public Response(String row) {
-            this.partitionKey = PARTITION;
-            this.rowKey = row;
-            this.address = row;
-        }
-    }
-
+//    public static class Response extends TableServiceEntity {
+//        public String response;
+//        public String address;
+//        public String recipient;
+//        public String modelType;
+//
+//        public String getResponse() {
+//            return this.response;
+//        }
+//
+//        public void setResponse(String m) {
+//            this.response = m;
+//        }
+//
+//        public String getAddress() {
+//            return this.address;
+//        }
+//
+//        public void setAddress(String a) {
+//            this.address = a;
+//        }
+//
+//        public String getRecipient() {
+//            return this.recipient;
+//        }
+//
+//        public void setRecipient(String r) {
+//            this.recipient = r;
+//        }
+//
+//        public String getModelType() { return this.recipient; }
+//
+//        public void setModelType(String r) { this.modelType = r; }
+//
+////        public Response() {
+////            this.partitionKey = PARTITION;
+////            this.rowKey = "ROW";
+////        }
+////
+////        public Response(String row) {
+////            this.partitionKey = PARTITION;
+////            this.rowKey = row;
+////            this.address = row;
+////        }
+//    }
+//
     private String[] getEnabledContactsNumbers() {
         Map<String, ?> map = getSharedPreferences(getString(R.string.shared_pref_key),
                 MODE_PRIVATE).getAll();
